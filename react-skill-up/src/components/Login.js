8f525602748ca865ad82ac3dbf0d456f195eba63
arrdamian <arrdamian@icloud.com>
2023-02-01 19:34:30 -0300

    Start component "Login"

---
+++ react-skill-up/src/components/Login.js
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import swAlert from '@sweetalert/with-react';
+
+export default function Login() {
+
+    const submitHandler = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if(email === '' || password === '') {
+            swAlert(<h3>Los campos no pueden estar vacios</h3>);
+            return;
+        }
+
+        if(email !== '' && !regexEmail.test(email)) {
+            swAlert(<h3>Debes escribir una direccion de correo electronico valida</h3>);
+            return;
+        }
+
+        if(email === 'challenge@alkemy.org' && password !== 'react') {
+            swAlert(<h3>Credenciales invalidas</h3>);
+            return;
+        }
+
+        axios
+            .post('http://challenge-react.alkemy.org', { email, password })
+            .then(res => {
+                swAlert(<h3>Perfecto, ingresaste correctamente</h3>);
+                console.log(res.data);
+                const token = res.data.token;
+                localStorage.setItem('token', token);
+            })
+    }
+
+  return (
+    <>
+        <h2>Formulario de login</h2>
+        <form onSubmit={submitHandler}>
+            <label>
+                <span>Correo electronico:</span> <br />
+                <input type="email" name="email" />
+            </label>
+            <br />
+            <label>
+                <span>Contraseña:</span> <br />
+                <input type="password" name="password" />
+            </label>
+            <br />
+            <button type="submit">Ingresar</button>
+        </form>
+    </>
+  )
+}
