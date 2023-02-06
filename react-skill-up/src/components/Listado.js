0f5906d8a0b29a8f36ddb5cbf54418de6e03c248
arrdamian <arrdamian@icloud.com>
2023-02-06 18:34:45 -0300

    Rendering of the data in the component

--- react-skill-up/src/components/Listado.js
+++ react-skill-up/src/components/Listado.js
@@ -28,16 +28,21 @@ export default function Listado() {
             { !token && <Navigate to='/' /> }
             
             <div className="row">
-                <div className="col-3" style={{border: '1px solid red'}}>
-                    <div className="card">
-                        <img src="..." className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <Link to="/" className="btn btn-primary">Ver datalle</Link>
+                { moviesList.map((oneMovie, idx) => {
+                    return (
+                        <div className="col-3" key={idx}>
+                            <div className="card my-4">
+                                <img src={`https://image.tmdb.org/t/p/w500${oneMovie.poster_path}`} className="card-img-top" alt={oneMovie.title} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{ oneMovie.title.substring(0, 30) }...</h5>
+                                    <p className="card-text">{ oneMovie.overview.substring(0, 100) }...</p>
+                                    <Link to="/" className="btn btn-primary">Ver datalle</Link>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    )
+                }) }
+
             </div>
         </>
     )
