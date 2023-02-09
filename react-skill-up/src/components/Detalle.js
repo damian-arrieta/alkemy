b83ac3391ef77dfa24fe87029fe791bcfe72a34b
arrdamian <arrdamian@icloud.com>
2023-02-09 18:44:40 -0300

    Got the data from the API for the Detalle component

--- react-skill-up/src/components/Detalle.js
+++ react-skill-up/src/components/Detalle.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
+import axios from 'axios';
+import swAlert from '@sweetalert/with-react';
 
 export default function Detalle() {
 
@@ -8,10 +10,25 @@ export default function Detalle() {
     let query = new URLSearchParams(window.location.search);
     let movieID = query.get('movieID');
 
+    useEffect(() => {
+      const endPoint = 'https://api.themoviedb.org/3/movie/${movieID}?api_key=6d01a6cc72de7f4787d584b8986e2172&language=es-ES';
+      axios.get(endPoint).then(response => {
+        const movieData = response.data;
+        console.log(movieData);
+      })
+      .catch(error => {
+        swAlert(<h2>Hubo errores. Intenta mas tarde</h2>);
+      })
+    }, [movieID]);
+
     return (
     <>
         { !token && <Navigate to='/' /> }
-        <div>Detalle</div>
+        <h2>Detalle de la película</h2>
+        <div className="row">
+          <div className="col-4"></div>
+          <div className="col-8"></div>
+        </div>
     </>
   )
 }
